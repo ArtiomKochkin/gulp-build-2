e358a378f960fc00c94d347ad58f9e1fe83a3314
ArtiomKochkin <artiom.kochkin@yandex.ru>
2024-02-19 10:22:09 +0700

    Feat: add typescript to the pack and .gitignore

--- src/ts/modules/functions.ts
+++ src/ts/modules/functions.ts
@@ -1,7 +1,7 @@
 // проверка поддержки webp, добавление соотв. классов для HTML
-export function isWebp() {
+export function isWebp(): void {
     // проверка поддержки webp
-    function testWebp(callback) {
+    function testWebp(callback: any) {
         let webP = new Image();
         webP.onload = webP.onerror = function () {
             callback(webP.height == 2);
@@ -9,11 +9,11 @@ export function isWebp() {
         webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
     }
     // добавление класса _webp или _no-webp для HTML
-    testWebp(function (support) {
+    testWebp(function (support: any) {
         if (support == true) {
-            document.querySelector('body').classList.add('webp');
+            document.querySelector('body')?.classList.add('webp');
             }else{
-            document.querySelector('body').classList.add('no-webp');
+            document.querySelector('body')?.classList.add('no-webp');
         }
     });
 }
